05cf7e4b6c3f20c9314eb0d444b721b8d44edd88
NgunyiGachie <antogachie@gmail.com>
2024-03-20 12:30:49 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+
+function random() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+const max = Math.max(...numbers);
+
+module.exports = { multiply, random, mod, max };
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -11,12 +11,15 @@ describe('index.js', function() {
       expect(multiply).to.eq(62)
     });
   });
+  
   describe('random', function() {
     it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
+      const randomNumber = random();
+      expect(Number.isInteger(randomNumber)).to.be.true;
+      expect(randomNumber).to.be.above(0);
     });
   });
+  
   describe('mod', function() {
     it("is an equation that calculates a remainder that is equal to 4", function() {
       expect(num3).to.be.a('number')
